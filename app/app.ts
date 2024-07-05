9a3efc7c81fe1036c36fc83f073519be23c41883
Junius Lekgwara <juniusl@bbd.co.za>
2024-07-05 11:25:11 +0200

    increased limit

--- app/app.ts
+++ app/app.ts
@@ -8,7 +8,9 @@ import { routes } from './routes';
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({
+  limit: '50mb'
+}));
 
 export const api = async (): Promise<Express> => {
 
